024f0ef87b79132d0c16577aa5c972d08fc336b5
Esteban <=>
2022-10-13 13:40:12 +0200

    Setup

---
+++ site/src/router/index.js
@@ -0,0 +1,21 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const routes = [
+    {
+        path: '/',
+        name: 'home',
+        component: HomeView
+    }, 
+    {
+        path: "/adminDashboard", 
+        name: ""
+    }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
