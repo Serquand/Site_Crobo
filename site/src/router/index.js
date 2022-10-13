eb7ca0d99963a0f1a223c0266395e74c38693e76
Esteban <=>
2022-10-13 14:09:23 +0200

    Router front

--- site/src/router/index.js
+++ site/src/router/index.js
@@ -1,5 +1,12 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
+import AdminDashboardView from "../views/AdminDashboardView";
+import WatchGamesView from "../views/WatchGamesView";
+import CreateGameView from "../views/CreateGameView";
+import Game from "../views/Game";
+import EventView from "../views/EventView";
+import ProjectView from "../views/ProjectView";
+import RankingView from "../views/RankingView";
 
 const routes = [
     {
@@ -9,7 +16,42 @@ const routes = [
     }, 
     {
         path: "/adminDashboard", 
-        name: ""
+        name: "Admin dashboard",
+        component: AdminDashboardView, 
+        meta: { requireAdmin: true }, 
+        children: [
+            {
+                path: "watchGames", 
+                name: "Watch games", 
+                component: WatchGamesView
+            }, 
+            {
+                path: "createGames", 
+                name: "Create games", 
+                component: CreateGameView
+            },
+        ]
+    }, 
+    {
+        path: "/game", 
+        name: "Game", 
+        component: Game, 
+        meta: { requireAuth: true }
+    }, 
+    {
+        path: "/event", 
+        name: "Event ", 
+        component: EventView, 
+    }, 
+    {
+        path: '/project', 
+        name: "Project", 
+        component: ProjectView, 
+    }, 
+    {
+        path: "/ranking/:user", 
+        name: "Ranking", 
+        component: RankingView
     }
 ]
 
---
+++ site/src/views/AdminDashboardView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="admin-dashboard-view"></div>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+</style>
